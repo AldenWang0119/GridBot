aee3d564958936bea881a603ad2cb27888c170d8
Alden <aldenwangf2e@gmail.com>
2024-11-28 12:50:28 +0800

    Add GridServer

--- GridBot/Program.cs
+++ GridBot/Program.cs
@@ -1,20 +1,10 @@
 ﻿using GridBot.Models;
-using GridBot.Services;
-using GridBot.Strategies;
-using Microsoft.Extensions.Configuration;
+using GridBot.Server;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .Build();
-
-        var orderService = new OrderService(configuration);
-
-        // 初始化網格配置
         var gridConfig = new GridConfig
         {
             Symbol = "SOLUSDT",
@@ -24,19 +14,7 @@ class Program
             InitialFunds = 100m
         };
 
-        var gridStrategy = new GridStrategy(gridConfig, orderService);
-
-        // 啟動 WebSocket 獲取實時價格
-        var webSocketService = new WebSocketService(gridConfig.Symbol.ToLower());
-        webSocketService.OnPriceUpdate += (currentPrice) =>
-        {
-            gridStrategy.HandlePriceChange(currentPrice);
-        };
-
-        webSocketService.Connect();
-
-        Console.WriteLine("按下任意鍵退出...");
-        Console.ReadKey();
-        webSocketService.Close();
+        GridServer gridServer = new GridServer();
+        gridServer.PlaceSpotGrid(gridConfig);
     }
 }
---
+++ GridBot/Server/GridServer.cs
@@ -0,0 +1,33 @@
+﻿using GridBot.Models;
+using GridBot.Services;
+using GridBot.Strategies;
+using Microsoft.Extensions.Configuration;
+
+namespace GridBot.Server
+{
+    public class GridServer
+    {
+        public void PlaceSpotGrid(GridConfig gridConfig)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var orderService = new OrderService(configuration);
+            var gridStrategy = new GridStrategy(gridConfig, orderService);
+
+            var webSocketService = new WebSocketService(gridConfig.Symbol.ToLower());
+            webSocketService.OnPriceUpdate += (currentPrice) =>
+            {
+                gridStrategy.HandlePriceChange(currentPrice);
+            };
+
+            webSocketService.Connect();
+
+            Console.WriteLine("按下任意鍵退出...");
+            Console.ReadKey();
+            webSocketService.Close();
+        }
+    }
+}
